9f0de3f092e06b7c86739dd10acb16ad2e05e590
moises-herrera <moisesherrera037@gmail.com>
2023-02-15 19:25:16 -0500

    minor change

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const path = require("path");
 const { dbConnection } = require('./database/config');
 
 const app = express();
