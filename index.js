c86ea4913ff599bdce3e10dfe84831aa7c2ca407
moises-herrera <moisesherrera037@gmail.com>
2023-02-15 19:24:27 -0500

    add public files

--- index.js
+++ index.js
@@ -26,6 +26,11 @@ app.use('/api/doctors', require('./routes/doctors'));
 app.use('/api/all', require('./routes/searches'));
 app.use('/api/upload', require('./routes/uploads'));
 
+// Handle routes
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public/index.html"));
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
